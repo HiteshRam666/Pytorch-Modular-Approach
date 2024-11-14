77210027131f4e5f2e01aa972cb73b26ff032e50
Hitesh Ram <116026459+HiteshRam666@users.noreply.github.com>
2024-11-15 04:54:23 +0530

    Add files via upload

---
+++ model_builder.py
@@ -0,0 +1,35 @@
+
+"""
+Contains PyTorch model code to instantiate a TinyVGG model.
+"""
+
+import torch
+from torch import nn
+
+class TinyVGG(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(input_shape, hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(hidden_units * 13 * 13, output_shape)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.classifier(x)
+        return x
